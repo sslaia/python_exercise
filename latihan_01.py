a9fa0bc3d3e85525415c322a5bdce36d5805bafe
Sirus Laia <laia.sirus@gmail.com>
2021-07-14 21:01:49 +0100

    Create latihan_01.py

---
+++ latihan_01.py
@@ -0,0 +1,6 @@
+// Coba perbaiki kesalahan sintaks kode berikut:
+
+print(Ini sekedar latihan manipulasi apa yang disebut string")
+print("Tanda " + "digunakan untuk menggabungkan dua potongan kalimat")
+  print('misalnya print("Ya'ahowu " + "banuada")')
+print(("Garis baru bisa dibuat dengan tanda garis miring terbalik dan n.")
